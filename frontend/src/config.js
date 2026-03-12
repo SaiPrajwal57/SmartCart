47ed42f9a18c466fc0ad715ede97e473c026c052
Sai Prajwal R Gupta <154576061+SaiPrajwal57@users.noreply.github.com>
2026-03-12 19:35:00 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 // frontend/src/config.js
-export const API_BASE_URL = import.meta.env.VITE_API_URL || '';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://smartcart-v1ms.onrender.com';
